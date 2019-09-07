973f491322b4d09b9d93a43c97671a5f29c4d661
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-07 19:31:43 -0400

    Combine Reducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from 'redux';
 
-import cartReducer from './modules/cart/reducer';
+import rootReducer from './modules/rootReducer';
 
-const store = createStore(cartReducer);
+const store = createStore(rootReducer);
 
 export default store;
