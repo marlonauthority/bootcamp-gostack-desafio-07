784e95e0cc4dd7795e38807e30946a526b94295c
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-07 21:49:47 -0400

    Produto duplicado

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,3 +1,5 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   // console.tron.log('teste');
   // console.tron.log(action);
@@ -5,13 +7,19 @@ export default function cart(state = [], action) {
 
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [
-        ...state,
-        {
-          ...action.product,
-          amount: 1,
-        },
-      ];
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(
+          product => product.id === action.product.id
+        );
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({
+            ...action.product,
+            amount: 1,
+          });
+        }
+      });
     default:
       return state;
   }
