1aecfa4e059c601cd341a81852ff6cb7b713ff7a
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-09 14:38:48 -0400

    Estoque na edicao

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,4 +1,5 @@
 import { call, put, all, takeLatest, select } from 'redux-saga/effects';
+import { Alert } from 'react-native';
 import { formatPrice } from '../../../util/format';
 import api from '../../../services/api';
 import { addToCartSuccess, updateAmount } from './actions';
@@ -8,8 +9,19 @@ function* addToCart({ id }) {
     state.cartReducer.find(p => p.id === id)
   );
 
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    Alert.alert('Quantidade requisitada fora de estoque');
+    return;
+  }
+
   if (productExists) {
-    const amount = productExists.amount + 1;
     yield put(updateAmount(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
