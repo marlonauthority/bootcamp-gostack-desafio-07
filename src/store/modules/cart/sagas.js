a140c4f5bdb0dfc43a1f4e7a42c30fab56c1b967
marlondck <15050029+marlondck@users.noreply.github.com>
2019-09-09 14:50:21 -0400

    Estoque na alteracao

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -2,7 +2,7 @@ import { call, put, all, takeLatest, select } from 'redux-saga/effects';
 import { Alert } from 'react-native';
 import { formatPrice } from '../../../util/format';
 import api from '../../../services/api';
-import { addToCartSuccess, updateAmount } from './actions';
+import { addToCartSuccess, updateAmountSuccess } from './actions';
 
 function* addToCart({ id }) {
   const productExists = yield select(state =>
@@ -22,7 +22,7 @@ function* addToCart({ id }) {
   }
 
   if (productExists) {
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
 
@@ -35,4 +35,22 @@ function* addToCart({ id }) {
     yield put(addToCartSuccess(data));
   }
 }
-export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
+
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+
+  if (amount > stockAmount) {
+    Alert.alert('Quantidade requisitada fora de estoque');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+}
+export default all([
+  takeLatest('@cart/ADD_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
+]);
